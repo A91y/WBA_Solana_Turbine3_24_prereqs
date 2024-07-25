dcf390fc847f949de7633e1a3be5236868b88744
A91y <65825207+A91y@users.noreply.github.com>
2024-07-25 16:26:43 +0530

    feat: added transfer

---
+++ airdrop/transfer.ts
@@ -0,0 +1,69 @@
+import {
+  Transaction,
+  SystemProgram,
+  Connection,
+  Keypair,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../dev-wallet.json";
+
+// Import our dev wallet keypair from the wallet file
+const from = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+// Define our WBA public key
+const to = new PublicKey("HX1TPzh21wV1SFaENyu2YuAWzLfTL7ADjEtcNNdTCXiW");
+
+//Create a Solana devnet connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+(async () => {
+  try {
+    // Get balance of dev wallet
+    const balance = await connection.getBalance(from.publicKey);
+    // Create a test transaction to calculate fees
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance,
+      })
+    );
+
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash("confirmed")
+    ).blockhash;
+    transaction.feePayer = from.publicKey;
+
+    // Calculate exact fee rate to transfer entire SOL amount out of account minus fees
+    const fee =
+      (
+        await connection.getFeeForMessage(
+          transaction.compileMessage(),
+          "confirmed"
+        )
+      ).value || 0;
+
+    // Remove our transfer instruction to replace it
+    transaction.instructions.pop();
+
+    // Now add the instruction back with correct amount of lamports
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance - fee,
+      })
+    );
+
+    // Sign transaction, broadcast, and confirm
+    const signature = await sendAndConfirmTransaction(connection, transaction, [
+      from,
+    ]);
+    console.log(`Success! Check out your TX here:
+        https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+        // https://explorer.solana.com/tx/4vnU2yY98VvMuo4ZtKkyusMiXQYWnXbCe2yh8s3vdr1XDpLGM3xcLGfe2Y5RmUW8L5p3Zu6WSWFTkDJN4K5pAfDX?cluster=devnet
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
+})();
