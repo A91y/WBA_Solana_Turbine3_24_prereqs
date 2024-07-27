987a782969f413f4b7b437942937b8d1e8840159
A91y <65825207+A91y@users.noreply.github.com>
2024-07-27 08:04:08 +0530

    feat: added airdrop fn

--- airdrop_rust/src/lib.rs
+++ airdrop_rust/src/lib.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 mod tests {
     use bs58;
+    use solana_client::rpc_client::RpcClient;
     use solana_sdk::{
         pubkey::Pubkey,
-        signature::{Keypair, Signer},
+        signature::{Keypair, Signer, read_keypair_file},
     };
     use std::io::{self, BufRead};
     #[test]
@@ -19,7 +20,24 @@ mod tests {
         println!("{:?}", kp.to_bytes());
     }
     #[test]
-    fn airdop() {}
+    fn airdop() {
+        const RPC_URL: &str = "https://api.devnet.solana.com";
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+
+        // Connect to Solana Devnet RPC Client
+        let client = RpcClient::new(RPC_URL);
+
+        // We're going to claim 2 devnet SOL tokens (2 billion lamports)
+        match client.request_airdrop(&keypair.pubkey(), 2_000_000_000u64) {
+            Ok(s) => {
+                println!("Success! Check out your TX here:");
+                println!("https://explorer.solana.com/tx/{}?cluster=devnet", s.to_string());
+                // https://explorer.solana.com/tx/iF8ugwXJzNEooHQtbqSvGE6B9rGt77DWfhkKWSraxUFtEc8BVsZe6av1pvHpdEbuAASB4HDwxm1BnxgU1PSdv66?cluster=devnet
+            },
+            Err(e) => println!("Oops, something went wrong: {}", e.to_string()),
+        }
+    }
     #[test]
     fn transfer_sol() {}
     #[test]
